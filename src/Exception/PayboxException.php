e8b86c2f6f79ebf5255ab0be0c5119d6298c7e11
Sullivan SENECHAL <soullivaneuh@gmail.com>
2016-04-22 20:58:18 +0200

    Improve PayboxException message
    Show Paybox response code.

--- src/Exception/PayboxException.php
+++ src/Exception/PayboxException.php
@@ -7,4 +7,13 @@ namespace Nexy\PayboxDirect\Exception;
  */
 final class PayboxException extends RuntimeException
 {
+    /**
+     * {@inheritdoc}
+     */
+    public function __construct($message = '', $code = 0, \Exception $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+
+        $this->message = sprintf('%05d: %s', $code, $message);
+    }
 }
