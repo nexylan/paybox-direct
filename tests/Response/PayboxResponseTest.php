bffabdb749525d590570a4f985a181448c8fa569
Sullivan SENECHAL <soullivaneuh@gmail.com>
2016-04-22 12:29:03 +0200

    Add PayboxResponse tests (#9)

---
+++ tests/Response/PayboxResponseTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Nexy\PayboxDirect\Tests\Response;
+
+use Nexy\PayboxDirect\Response\PayboxResponse;
+
+/**
+ * @author Sullivan Senechal <soullivaneuh@gmail.com>
+ */
+final class PayboxResponseTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var PayboxResponse
+     */
+    private $response;
+
+    protected function setUp()
+    {
+        $this->response = new PayboxResponse([
+            'CODEREPONSE' => 1337,
+            'COMMENTAIRE' => 'French keywords are bad, and you should feel bad!',
+        ]);
+    }
+
+    public function testValidGetter()
+    {
+        $this->assertSame(1337, $this->response->getCode());
+        $this->assertSame('French keywords are bad, and you should feel bad!', $this->response->getComment());
+    }
+
+    public function testNotExistsProperty()
+    {
+        $this->assertNull($this->response->getDiscount());
+    }
+
+    public function testNotExistsGetter()
+    {
+        $this->expectException(\BadMethodCallException::class);
+        $this->expectExceptionMessage('Undefined method getFake');
+
+        $this->response->getFake();
+    }
+}
