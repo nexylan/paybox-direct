79a9cf820a214a176488c8429f17fa630bfc6591
Sullivan SENECHAL <soullivaneuh@gmail.com>
2016-04-27 18:08:08 +0200

    Remove not needed final methods

--- tests/Request/CancelRequestTest.php
+++ tests/Request/CancelRequestTest.php
@@ -12,7 +12,7 @@ final class CancelRequestTest extends AbstractNumberedReferencedTransactionReque
     /**
      * {@inheritdoc}
      */
-    final protected function getPreviousResponse($amount)
+    protected function getPreviousResponse($amount)
     {
         $request = new AuthorizeAndCaptureRequest(
             $this->generateReference(),
