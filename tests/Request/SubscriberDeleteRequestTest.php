e6d9e5183b537e097a7f791de438b500fe23d9fa
Sullivan SENECHAL <soullivaneuh@gmail.com>
2016-05-03 17:23:00 +0200

    Add missing header comment (#44)

--- tests/Request/SubscriberDeleteRequestTest.php
+++ tests/Request/SubscriberDeleteRequestTest.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the Nexylan packages.
+ *
+ * (c) Nexylan SAS <contact@nexylan.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Nexy\PayboxDirect\Tests\Request;
 
 use Nexy\PayboxDirect\Exception\PayboxException;
