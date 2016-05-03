6b9e1f0801902cfedea62ca65e11e62811461cf1
Sullivan SENECHAL <soullivaneuh@gmail.com>
2016-05-03 16:06:42 +0200

    Add missing header comment

--- tests/HttpClient/AbstractHttpClientTest.php
+++ tests/HttpClient/AbstractHttpClientTest.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the Nexylan packages.
+ *
+ * (c) Nexylan SAS <contact@nexylan.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Nexy\PayboxDirect\Tests\HttpClient;
 
 use Nexy\PayboxDirect\HttpClient\AbstractHttpClient;
